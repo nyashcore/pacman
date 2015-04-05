e53c6f19ab337e8fb744b30a3bdc360e1530ff1e
Gadjiev Said <gsarma.1995@mail.ru>
2015-04-05 13:08:14 +0300

    13:08 05.04.2015

--- Classes/HelloWorldScene.cpp
+++ Classes/HelloWorldScene.cpp
@@ -54,7 +54,7 @@ bool HelloWorld::init()
     // add a label shows "Hello World"
     // create and initialize a label
 
-    auto label = Label::createWithTTF("Hello, Bitches!", "fonts/Marker Felt.ttf", 24);
+    auto label = Label::createWithTTF("Level 1", "fonts/Marker Felt.ttf", 24);
 
     // position the label on the center of the screen
     label->setPosition(Vec2(origin.x + visibleSize.width/2,
@@ -64,13 +64,13 @@ bool HelloWorld::init()
     this->addChild(label, 1);
 
     // add "HelloWorld" splash screen"
-    auto sprite = Sprite::create("HelloWorld.png");
+    //auto sprite = Sprite::create("HelloWorld.png");
 
     // position the sprite on the center of the screen
-    sprite->setPosition(Vec2(visibleSize.width/2 + origin.x, visibleSize.height/2 + origin.y));
+    //sprite->setPosition(Vec2(visibleSize.width/2 + origin.x, visibleSize.height/2 + origin.y));
 
     // add the sprite as a child to this layer
-    this->addChild(sprite, 0);
+    //this->addChild(sprite, 0);
 
     // load the Sprite Sheet
     auto spritecache = SpriteFrameCache::getInstance();
