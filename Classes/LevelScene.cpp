648ea4f210fbc818612fd3603b78abd7165e9999
Gadjiev Said <gsarma.1995@mail.ru>
2015-04-18 14:11:46 +0300

    Added uml diogram

--- Classes/LevelScene.cpp
+++ Classes/LevelScene.cpp
@@ -29,7 +29,8 @@ bool Level::init()
     labelConfig.outlineSize = 0;
     labelConfig.customGlyphs = nullptr;
     labelConfig.distanceFieldEnabled = false;
-     auto map = TMXTiledMap::create("map/pacman.tmx");
+
+    auto map = TMXTiledMap::create("map/pacman.tmx");
     addChild(map, 0, 99);
     Vector<MenuItem*> MenuItems;
     auto closeItem = MenuItemImage::create(
