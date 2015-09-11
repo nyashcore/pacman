c8035869febc983033b6f092554bf48e7c099efa
Ivankov Igor <ivankov.i.f@gmail.com>
2015-09-11 16:39:19 +0300

    added patterns

---
+++ Classes/WinScene.cpp
@@ -0,0 +1,82 @@
+#include "WinScene.h"
+#include "LevelScene.h"
+USING_NS_CC;
+
+Scene* Win::createScen()
+{
+    auto scene = Scene::create();
+    auto layer = Win::create();
+    scene->addChild(layer);
+    return scene;
+}
+
+bool Win::init()
+{
+    if ( !Layer::init() )
+    {
+        return false;
+    }
+
+    Size visibleSize = Director::getInstance()->getVisibleSize();
+    Vec2 origin = Director::getInstance()->getVisibleOrigin();
+
+    TTFConfig labelConfig;
+    labelConfig.fontFilePath = "fonts/Marker Felt.ttf";
+    labelConfig.fontSize = 24;
+    labelConfig.glyphs = GlyphCollection::DYNAMIC;
+    labelConfig.outlineSize = 0;
+    labelConfig.customGlyphs = nullptr;
+    labelConfig.distanceFieldEnabled = false;
+
+    Vector<MenuItem*> MenuItems;
+    auto closeLabel = Label::createWithTTF(labelConfig, "Quit");
+    auto closeItem = MenuItemLabel::create(
+                                           closeLabel,
+                                           CC_CALLBACK_1(Win::menuCloseCallback, this));
+    closeItem->setPosition(Vec2(origin.x + visibleSize.width/2,
+                                    origin.y + visibleSize.height/3 - 30));
+	MenuItems.pushBack(closeItem);
+    auto restartLabel1 = Label::createWithTTF(labelConfig, "Restart");
+    auto restartItem = MenuItemLabel::create(
+                                            restartLabel1,
+                                            CC_CALLBACK_1(Win::menuRestartCallback, this));
+    restartItem->setPosition(Vec2(origin.x + visibleSize.width/2,
+                                    origin.y + visibleSize.height/3));
+	MenuItems.pushBack(restartItem);
+
+    auto menu = Menu::createWithArray(MenuItems);
+    menu->setPosition(Vec2::ZERO);
+    this->addChild(menu, 1);
+
+    auto label = Label::createWithTTF(labelConfig, "Game Over!");
+    label->setPosition(Vec2(origin.x + visibleSize.width/2,
+                            origin.y + visibleSize.height - label->getContentSize().height));
+    label->enableOutline(Color4B::GRAY, 1);
+    this->addChild(label, 1);
+
+    auto sprite = Sprite::create("HelloWorld.png");
+    sprite->setPosition(Vec2(visibleSize.width/2 + origin.x, visibleSize.height/2 + origin.y));
+    this->addChild(sprite, 0);
+
+    return true;
+}
+
+
+void Win::menuCloseCallback(Ref* pSender)
+{
+#if (CC_TARGET_PLATFORM == CC_PLATFORM_WP8) || (CC_TARGET_PLATFORM == CC_PLATFORM_WINRT)
+	MessageBox("You pressed the close button. Windows Store Apps do not implement a close button.","Alert");
+    return;
+#endif
+
+    Director::getInstance()->end();
+
+#if (CC_TARGET_PLATFORM == CC_PLATFORM_IOS)
+    exit(0);
+#endif
+}
+void Win::menuRestartCallback(Ref* pSender)
+{
+    auto scene = Level::createScene(1);
+    Director::getInstance()->replaceScene(TransitionFade::create(1, scene));
+}
---
+++ Classes/WinScene.h
@@ -0,0 +1,26 @@
+#ifndef __WIN_SCENE_H__
+#define __WIN_SCENE_H__
+
+#include "cocos2d.h"
+#include <iostream>
+
+class Win : public cocos2d::Layer
+{
+public:
+    // there's no 'id' in cpp, so we recommend returning the class instance pointer
+    static cocos2d::Scene* createScen();
+
+    // Here's a difference. Method 'init' in cocos2d-x returns bool, instead of returning 'id' in cocos2d-iphone
+    virtual bool init();
+
+    // a selector callback
+    void menuCloseCallback(cocos2d::Ref* pSender);
+    void menuRestartCallback(cocos2d::Ref* pSender);
+//    void menuRestartCallback(cocos2d::Ref* pSender);
+
+    // implement the "static create()" method manually
+    CREATE_FUNC(Win);
+};
+
+#endif // __GameOver_SCENE_H__
+
